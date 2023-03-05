68555e7d8a42a32e99cbf596eebbcc970fb95504
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-05 15:19:34 -0300

    Add project files.

---
+++ Exercicio_MaisEmContaEntreAlcoolEGasoina/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double preco_alcool, preco_gas;
+
+        Console.WriteLine("Digite o preco alcool: ");
+        preco_alcool = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o preco gasolina: ");
+        preco_gas = double.Parse(Console.ReadLine());
+
+
+        if (preco_alcool / preco_gas <= 0.72)
+        {
+            Console.WriteLine("compre gasolina");
+        }
+        else
+        {
+            Console.WriteLine("compre alcool");
+        }
+    }
+}
